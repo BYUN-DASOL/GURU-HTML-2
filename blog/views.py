1925423722fc1636ee7bc11b1b1bcf827b36b466
chaeeun-Han <becky2525@naver.com>
2020-08-15 19:23:43 +0900

    category_detail

--- blog/views.py
+++ blog/views.py
@@ -20,22 +20,18 @@ class PostDetail(DetailView):
     model = Post
 
 
-class PostListByCategory(ListView):
-    def get_queryset(self):
-        slug = self.kwargs['slug']
-        category = Category.objects.get(slug=slug)
+def PostListByCategory(request, slug):
+    category = Category.objects.get(slug=slug)
+    category_post = Post.objects.filter(category=category)
 
-        return Post.objects.filter(category=category).order_by('-created')
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super(type(self), self).get_context_data(**kwargs)
-        context['category_list'] = Category.objects.all()
-
-
-        slug = self.kwargs['slug']
-        category = Category.objects.get(slug=slug)
-
-        return context
+    return render(
+        request,
+        'blog/category_detail.html',
+        {
+            'category': category,
+            'category_post': category_post,
+         }
+    )
 
 # def document_list(request):
 #     # documents = Document.objects.all()
