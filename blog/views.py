33352844246c711e7764249be748d0e1848e1012
chaeeun-Han <68267916+chaeeun-Han@users.noreply.github.com>
2020-08-18 23:01:56 +0900

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,4 @@
-import math
-
 from django.core.paginator import Paginator
-from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render
 from .models import Post, Category
 from django.views.generic import ListView, DetailView
@@ -16,7 +13,6 @@ class PostList(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(PostList, self).get_context_data(**kwargs)
         context['category_list'] = Category.objects.all()
-
         return context
 
 class PostSearch(PostList):
@@ -29,33 +25,19 @@ class PostSearch(PostList):
 class PostDetail(DetailView):
     model = Post
 
-
-
 def PostListByCategory(request, slug):
-    paginate_by = 4
     category = Category.objects.get(slug=slug)
     category_post = Post.objects.filter(category=category)
+
+    page = request.GET.get('page', '1')  # 페이지
+    paginator = Paginator(category_post, 3)
+    page_obj = paginator.get_page(page)
 
     return render(
         request,
         'blog/category_detail.html',
         {
             'category': category,
-            'category_post': category_post,
+            'category_post': page_obj,
          }
     )
-
-def index(request):
-    posts = Post.objects.all().order_by('-published_date')
-    paginator = Paginator(posts, 10)
-    page = request.GET.get('page')
-    contacts = paginator.get_page(page)
-    page_range = 5
-    current_block = math.ceil(int(page)/page_range)
-    start_block = (current_block-1) * page_range
-    end_block = start_block + page_range
-    p_range = paginator.page_range[start_block:end_block]
-    return render(request, 'blog/category_detail.html', {
-        'contacts': contacts,
-        'p_range': p_range,
-    })
