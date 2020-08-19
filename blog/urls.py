8962f950dfb518850d9e566fca51f79aba18895f
whitecastle20 <sjtjdwn20@gmail.com>
2020-08-19 22:52:56 +0900

    finish

--- blog/urls.py
+++ blog/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.urls import path,include
 from blog import views
 
 
 urlpatterns = [
+    path('blog/search/<str:q>/', views.PostSearch.as_view()),
     path('blog/category/<str:slug>/', views.PostListByCategory),
     path('blog/<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
